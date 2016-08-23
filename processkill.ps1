8f5c2fffcc5da985ed7a4e62ad4cfe1dfe45d7ef
Dag hemphälä <dag@ddagre.com>
2016-08-23 22:15:52 +0200

    taskkill
    A little funny program i made with help from a few friends. It kills a
    specific program in this case hearthstone.exe on a local network if the
    user has admin permission on the local network.

---
+++ processkill.ps1
@@ -0,0 +1,17 @@
+﻿function Get-NetView {
+	switch -regex (NET.EXE VIEW) { "^\\\\(?<Name>\S+)\s+" {$matches.Name}}
+	}
+
+$lista3=Get-NetView
+$lista3 = $lista3[10..($lista3.Length+0)]
+
+foreach ($y in $lista3){
+    echo $y
+
+    if ($y -ne "LE6430-4HNVLX1" -and $y -ne "LE6430-5XPVLX1" -and $y -ne "LE6430-2PMVLX1" -and $y -ne "LE6430-54NVLX1" -and $y -ne "LE6430-JHQVLX1" -and $y -ne "LE6430-6WKVLX1" -and $y -ne "LE6430-43NVLX1" -and $y -ne "LE6430-33QVLX1" -and $y -ne "LE6430-6YPVLX1" -and $y -ne "LE6430-D3PVLX1" -and $y -ne "LE6430-29GVLX1" -and $y -ne "LE6430-DXNVLX1")
+    {
+        taskkill -s \\$y -im hearthstone.exe -f
+
+    }
+}
+pause
